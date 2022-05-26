47f23bfe350860e8eee88d15eb0f323b4a15ea46
YanLong-111 <2944972991@qq.com>
2022-05-26 15:45:34 +0800

    add flattener contract

---
+++ contracts/storages/MfiMerkleStorages.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract MfiMerkleStorages {
+
+    bytes32 public merkleRoot;
+
+    // blacklist users
+    mapping(address => bool)public blackListUser;
+
+}
